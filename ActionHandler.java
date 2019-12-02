87e2717db2f31078aa13cabef94191ce2b28276b
dianacocos15 <32218894+dianacocos15@users.noreply.github.com>
2019-12-02 16:38:49 +0000

    Add files via upload

--- ActionHandler.java
+++ ActionHandler.java
@@ -21,46 +21,48 @@ public class ActionHandler {
 	public String performAction(String command) {
 		command = command.replaceAll("\\s+","");
 		
-		int code = Integer.parseInt(command);
-		System.out.println("Code is : " +code);
+		String code = command;
+		System.out.println("Code is : " + code);
 		
+		int rotation = 0;
+		int distance = 0;
 		/*Rotate and correct action codes*/
-		if(code == 90) {
-			correct.rotateAndLineCorrect(90);
-			return "0";
+		switch (code) {
+			case "90": rotation = 90;
+				break;
+			case "-90": rotation = -90;
+				break;
+			case "180": rotation = 180;
+				break;
+			case "-180": rotation = -180;
+				break;
+			case "travel": distance = 1;
+				break;
+			default: return "1";
 		}
-		if(code == -90) {
-			correct.rotateAndLineCorrect(-90);
-			return "0";
-		}
-		if(code == 180) {
-			correct.rotateAndLineCorrect(180);
-			return "0";
-		}
+	
+		move.rotate(rotation);
+		move.travelCellDistance(distance);
+		
+		return "0";
 		
 		/*Cell localise action codes*/
-		if(code == 11) {
-			correct.lineLocalisationToLeft();
-			return "0";
-		}
-		if(code == 12) {
-			correct.lineLocalisationToRight();
-			return "0";
-		}
+//		if(code == 11) {
+//			correct.lineLocalisationToLeft();
+//			return "0";
+//		}
+//		if(code == 12) {
+//			correct.lineLocalisationToRight();
+//			return "0";
+//		}
 		
-		/*Move cells action codes*/
-		if(code <= 6 && code >=1) {
-			move.travelCellDistance(code);
-			return "0";
-		}
+		/*Move cells action codes*
 		
 		/*Color sense action code*/
-		if(code == 100) {
-			Color color = robot.getColor();
-			return ""+color;
-		}
-		
-		return "1";
+//		if(code == 100) {
+//			Color color = robot.getColor();
+//			return ""+color;
+//		}
 	}
 
 }
