e3070d0ed3d9b13ebf3ca14073c76efc24c24992
Msuhail19 <34750682+Msuhail19@users.noreply.github.com>
2019-12-02 17:02:57 +0000

    Add files via upload

--- ActionHandler.java
+++ ActionHandler.java
@@ -25,9 +25,11 @@ public class ActionHandler {
 		System.out.println("Code is : " + code);
 		
 		int rotation = 0;
-		int distance = 0;
+		int cell_distance = 0;
+		
 		/*Rotate and correct action codes*/
 		switch (code) {
+			/*Rotation statements*/
 			case "90": rotation = 90;
 				break;
 			case "-90": rotation = -90;
@@ -36,33 +38,53 @@ public class ActionHandler {
 				break;
 			case "-180": rotation = -180;
 				break;
-			case "travel": distance = 1;
-				break;
-			default: return "1";
 		}
-	
-		move.rotate(rotation);
-		move.travelCellDistance(distance);
 		
-		return "0";
+		/*Ensure rotation not 0 before travelling*/
+		if(rotation !=0) {
+			correct.rotateAndLineCorrect(rotation);
+			return "0";
+		}
 		
-		/*Cell localise action codes*/
-//		if(code == 11) {
-//			correct.lineLocalisationToLeft();
-//			return "0";
-//		}
-//		if(code == 12) {
-//			correct.lineLocalisationToRight();
-//			return "0";
-//		}
+		/*Travel statements*/
+		switch(code) {
+			case "1": cell_distance = 1;
+				break;
+			case "2": cell_distance = 2;
+				break;
+			case "3": cell_distance = 3;
+				break;
+			case "4": cell_distance = 4;
+				break;
+			case "5": cell_distance = 5;
+				break;
+			case "travel": cell_distance = 1;
+				break;
+		}
 		
-		/*Move cells action codes*
+		/*Ensure distance not 0 before travelling*/
+		if(cell_distance != 0) {
+			move.travelCellDistance(cell_distance);
+			return "0";
+		}
 		
-		/*Color sense action code*/
-//		if(code == 100) {
-//			Color color = robot.getColor();
-//			return ""+color;
-//		}
+		/*Additional commands*/
+		switch(code) {
+			/*Color sensor command*/
+			case "100":
+				Color color = robot.getColor();
+				return ""+color;
+			/*Localise left*/
+			case "11":
+				correct.lineLocalisationToLeft();
+				return "0";
+			/*Localise Right*/
+			case "12":
+				correct.lineLocalisationToRight();
+				return "0";
+		}
+		
+		return "1";
 	}
 
 }
